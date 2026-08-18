6e7f7c734ebee0e7ddedbeb48ba91b68789e7fe3
Slayer366 <andykratz@hotmail.com>
2026-08-18 05:18:15 -0500

    Update

--- Update-RG351P-dArkOS.sh
+++ Update-RG351P-dArkOS.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 clear
 
-UPDATE_DATE="08172026"
+UPDATE_DATE="08182026"
 LOG_FILE="/home/ark/update$UPDATE_DATE.log"
 UPDATE_DONE="/home/ark/.config/.update$UPDATE_DATE"
 
@@ -163,6 +163,45 @@ if [ ! -f "/home/ark/.config/.update08172026" ]; then
       sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.3200.10 /usr/lib/arm-linux-gnueabihf/libSDL2.so | tee -a "$LOG_FILE"
 
   touch "/home/ark/.config/.update08172026"
+
+fi
+
+
+if [ ! -f "/home/ark/.config/.update08182026" ]; then
+
+  printf "\nFix standalone mednafen controls for OSH controller \n" | tee -a "$LOG_FILE"
+  sudo rm -rf /dev/shm/*
+  sudo wget --no-check-certificate https://github.com/slayer366/darkos-rg351p/raw/main/08182026/darkosupdate08182026.zip -O /home/ark/darkosupdate08182026.zip -a "$LOG_FILE" || rm -f /home/ark/darkosupdate08182026.zip | tee -a "$LOG_FILE"
+  if [ -f "/home/ark/darkosupdate08182026.zip" ]; then
+    sudo unzip -X -o /home/ark/darkosupdate08182026.zip -d / | tee -a "$LOG_FILE"
+    sudo rm -v /home/ark/darkosupdate08182026.zip | tee -a "$LOG_FILE"
+  else
+    printf "\nThe update couldn't complete because the package did not download correctly.\nPlease retry the update again." | tee -a "$LOG_FILE"
+    sleep 3
+    echo $c_brightness > /sys/devices/platform/backlight/backlight/backlight/brightness
+    exit 1
+  fi
+
+
+      sudo chown -R ark:ark /opt/
+
+    printf "\nMake sure permissions for the ark home directory are set to 755\n" | tee -a "$LOG_FILE"
+      sudo chown -R ark:ark /home/ark
+      sudo chmod -R 755 /home/ark
+
+    printf "\nEnsure proper permissions are set for standalone mednafen \n" | tee -a "$LOG_FILE"
+      sudo chmod 777 /usr/local/bin/mednafen
+      sudo chmod 775 /home/ark/.mednafen/mednafen.cfg
+
+    printf "\nEnsure 64bit and 32bit SDL2 are still properly linked\n" | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2.so /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0 /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/aarch64-linux-gnu/libSDL2-2.0.so.0.3200.10 /usr/lib/aarch64-linux-gnu/libSDL2.so | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2.so /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0 /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so | tee -a "$LOG_FILE"
+      sudo ln -sfv /usr/lib/arm-linux-gnueabihf/libSDL2-2.0.so.0.3200.10 /usr/lib/arm-linux-gnueabihf/libSDL2.so | tee -a "$LOG_FILE"
+
+  touch "/home/ark/.config/.update08182026"
 
 fi
 
